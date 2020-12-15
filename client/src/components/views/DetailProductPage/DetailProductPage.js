827ce8ab3c23a867280f85d33101a79dabbaf4c0
Moon-Sangho <skym887@gmail.com>
2020-12-15 22:20:25 +0900

    Product Image 컴포넌트 생성 및 이미지 슬라이더 구현

--- client/src/components/views/DetailProductPage/DetailProductPage.js
+++ client/src/components/views/DetailProductPage/DetailProductPage.js
@@ -1,8 +1,26 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+
+import ProductImage from "./Sections/ProductImage";
+import ProductInfo from "./Sections/ProductInfo";
+
 import axios from "axios";
+import { Row, Col } from "antd";
+
+const Wrapper = styled.div`
+  width: 100%;
+  padding: 3rem 4rem;
+`;
+
+const ProductNameWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 10px;
+`;
 
 function DetailProductPage(props) {
   const productId = props.match.params.productId;
+  const [Product, setProduct] = useState({});
 
   useEffect(() => {
     axios
@@ -10,13 +28,28 @@ function DetailProductPage(props) {
       .then((res) => {
         if (res.data.success) {
           console.log(res.data);
+          setProduct(res.data.product[0]);
         } else {
           alert("상품 상세 정보 가져오기를 실패했습니다");
         }
       });
-  });
+  }, []);
 
-  return <div>DetailProductPage</div>;
+  return (
+    <Wrapper>
+      <ProductNameWrapper>
+        <h1>{Product.title}</h1>
+      </ProductNameWrapper>
+      <Row gutter={[16, 16]}>
+        <Col lg={12} sm={24}>
+          <ProductImage detail={Product} />
+        </Col>
+        <Col lg={12} sm={24}>
+          <ProductInfo />
+        </Col>
+      </Row>
+    </Wrapper>
+  );
 }
 
 export default DetailProductPage;
