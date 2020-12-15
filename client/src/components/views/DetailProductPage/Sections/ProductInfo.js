c8168866c58eea8908d14fe1a6436cc68ccb5472
Moon-Sangho <skym887@gmail.com>
2020-12-15 22:35:54 +0900

    Product Info 컴포넌트 생성 및 상품 상세정보 출력

--- client/src/components/views/DetailProductPage/Sections/ProductInfo.js
+++ client/src/components/views/DetailProductPage/Sections/ProductInfo.js
@@ -1,7 +1,35 @@
 import React from "react";
+import styled from "styled-components";
 
-function ProductInfo() {
-  return <div>ProductInfo</div>;
+import { Descriptions, Button } from "antd";
+
+const BtnWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 30px;
+`;
+
+function ProductInfo(props) {
+  const clickHandler = () => {};
+  return (
+    <div>
+      <Descriptions title="Product Info" bordered>
+        <Descriptions.Item label="Price">
+          {props.detail.price && props.detail.price.toLocaleString()}
+        </Descriptions.Item>
+        <Descriptions.Item label="Sold">{props.detail.sold}</Descriptions.Item>
+        <Descriptions.Item label="View">{props.detail.views}</Descriptions.Item>
+        <Descriptions.Item label="Description">
+          {props.detail.description}
+        </Descriptions.Item>
+      </Descriptions>
+      <BtnWrapper>
+        <Button size="large" shape="round" type="danger" onClick={clickHandler}>
+          Add to Cart
+        </Button>
+      </BtnWrapper>
+    </div>
+  );
 }
 
 export default ProductInfo;
