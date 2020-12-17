4380026c1af3b2647fcad2ad5c29dff1d6d7123a
Moon-Sangho <skym887@gmail.com>
2020-12-18 02:30:56 +0900

    카트에 담긴 상품 정보들을 데이터베이스에서 가져오기

--- server/routes/product.js
+++ server/routes/product.js
@@ -107,16 +107,25 @@ router.post("/products", (req, res) => {
 
 router.get("/products_by_id", (req, res) => {
   let type = req.query.type;
-  let productId = req.query.id;
+  let productIds = req.query.id;
+
+  if (type === "array") {
+    // id = 1234512512, 125r125121, 125r34rt5235 이런 형식을
+    // productIds = ['1234512512', '125r125121', '125r34rt5235'] 와 같은 형식으로 바꿈
+    let ids = req.query.id.split(",");
+    productIds = ids.map((item) => {
+      return item;
+    });
+  }
 
   // productId를 이용해서 DB에서 productId와 같은 상품의 정보를 가져온다.
-  Product.find({ _id: productId })
+  Product.find({ _id: { $in: productIds } })
     .populate("writer")
     .exec((err, product) => {
       if (err) {
         return res.status(400).send(err);
       } else {
-        return res.status(200).send({ success: true, product });
+        return res.status(200).send({ product });
       }
     });
 });
